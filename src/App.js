4887cae9f6d8318550d6de7d04912f1ab44940ff
Christy Hanson <chrsthnsn@gmail.com>
2023-07-05 21:37:07 -0600

    moved img folder

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Main from './components/Main';
@@ -9,21 +9,21 @@ import "./index.css"
 function App() {
   // default value is 'about'
   // 
-  const [page,setPage]=useState('about');
+  const [page, setPage] = useState('about');
   return (
     <div className="App">
-     <div className="content-box">
-      {/* send variable and function. header can see what page we are on. */}
-      <Header page={page} setPage={setPage}></Header> 
-      
-      {/* Main content of the page */}
-      {/* Main does not have the permission to set the variable values. It can only see what is stored in the page variable. */}
-      <Main page={page}></Main>
+      <div className="content-box">
+        {/* send variable and function. header can see what page we are on. */}
+        <Header page={page} setPage={setPage}></Header>
+
+        {/* Main content of the page */}
+        {/* Main does not have the permission to set the variable values. It can only see what is stored in the page variable. */}
+        <Main page={page}></Main>
 
 
-{/* Don't put anything here yet. */}
-<Footer></Footer>
-</div>
+        {/* Don't put anything here yet. */}
+        <Footer></Footer>
+      </div>
 
     </div>
   );
